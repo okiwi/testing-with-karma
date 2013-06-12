542c8aa0cd1e81e3ae60d613eb6f7ed992e2ef05
Mathieu Comandon <strycore@gmail.com>
2013-06-12 18:57:42 +0200

    Initial commit

---
+++ test/test2.js
@@ -0,0 +1,16 @@
+describe("A suite", function() {
+  it("contains spec 1", function() {
+    expect(true).toBe(true);
+  });
+  it("contains spec with an expectation", function() {
+    expect(true).toBe(true);
+  });
+});
+describe("A suite", function() {
+  it("contains spec 1", function() {
+    expect(true).toBe(true);
+  });
+  it("contains spec with an expectation", function() {
+    expect(true).toBe(true);
+  });
+});
